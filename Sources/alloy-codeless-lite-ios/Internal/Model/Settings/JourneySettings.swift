f59985e3bddda091637737cf8fe4418cc6a1e68b
Duncan <duncan@z1.digital>
2023-06-23 13:40:24 +0200

    feat: changed JourneySettings init to have two options

--- Sources/alloy-codeless-lite-ios/Internal/Model/Settings/JourneySettings.swift
+++ Sources/alloy-codeless-lite-ios/Internal/Model/Settings/JourneySettings.swift
@@ -15,10 +15,18 @@ public class JourneySettings {
     public var showDebugInfo: Bool = false
     init() {}
 
-    public init(journeyToken: String, journeyApplicationToken: String? = nil, entities: EntityData, production: Bool, showDebugInfo: Bool = false) {
+    public init(journeyToken: String, entities: EntityData, production: Bool, showDebugInfo: Bool = false) {
+        self.journeyToken = journeyToken
+        self.journeyApplicationToken = nil
+        self.entities = entities
+        self.production = production
+        self.showDebugInfo = showDebugInfo
+    }
+    
+    public init(journeyToken: String, journeyApplicationToken: String? = nil, production: Bool, showDebugInfo: Bool = false) {
         self.journeyToken = journeyToken
         self.journeyApplicationToken = journeyApplicationToken
-        self.entities = entities
+        self.entities = EntityData(entities: [], additionalEntities: false)
         self.production = production
         self.showDebugInfo = showDebugInfo
     }
